b8b0f59800548c60f7b517b2a5753246a0b71dd9
cmcclatchey <cmcclatchey@e116-m-18.mchenry.edu>
2016-10-06 13:06:12 -0500

    Javascript Example
    Web-115

---
+++ myscript.js
@@ -0,0 +1,27 @@
+/**
+ * Created by cmcclatchey on 10/4/16.
+ */
+
+function change01(){
+    document.getElementById("large").src="cat1.jpg";
+}
+
+function change02(){
+    document.getElementById("large").src="cat2.jpg";
+}
+
+function change03(){
+    document.getElementById("large").src="cat3.jpg";
+}
+
+function change04(){
+    document.getElementById("large").src="cat4.jpg";
+}
+
+function change05(){
+    document.getElementById("large").src="cat5.jpg";
+}
+
+function change06(){
+    document.getElementById("large").src="cat6.jpg";
+}
